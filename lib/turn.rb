6a562206515872e8da17b36ff1bb6aa78c0448e1
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-09-17 18:31:27 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,38 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i = x
+  index = x-1 
+end
+
+def valid_move?(board, index)
+  def position_taken?(board, index)
+    if board[index] == ""| board[index] == " " | board[index] == nil
+      return false
+    else 
+      return true
+    end
+  end
+  def valid_position?(index)
+    if index.between(0, 8) == true
+      return true
+    else
+      return false
+    end
+  end
+  if (position_taken?(board, index) == false) && (valid_position(index) == true)
+    return true
+  else
+    return false
+  end
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
